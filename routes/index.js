517755531c0a46d352c72ed863e850ff6ac8d2b7
Ryan Shin <1982sdr@hanmail.net>
2018-01-20 17:50:42 +0900

    안녕, node.js -> 안녕, express

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 // http://localhost:3000/ 요청시 다음 구문을 실행함. index.ejs를 보여줌.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '안녕, nodejs' });
+  res.render('index', { title: '안녕, express' });
 });
 
 module.exports = router;
